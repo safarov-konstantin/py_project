1518fdaebef5ea96b284dfd7b1c52c4202519ba3
Konstantin Safarov <safarov-konstantin@yandex.ru>
2023-09-12 22:50:37 +0300

    add main.py import function add_two_numbers from func writing the logic of the main function add subtracting_two_numbers in funcs.py

--- funcs.py
+++ funcs.py
@@ -3,3 +3,10 @@ def add_two_numbers(first_num, second_num):
     Сложение длвух чисел
     """
     return first_num + second_num
+
+
+def subtracting_two_numbers(first_num, second_num):
+    """
+    Вычитание чисел. Второе из первого
+    """
+    return second_num - first_num
